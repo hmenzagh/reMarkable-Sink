c52d63ac81fb7e9ec72c079d597905e46fe57dcc
Hugo Menzaghi <hmenzagh@MacBook-Pro-de-Hugo.local>
2020-11-17 00:30:34 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# ReMarkable-Sink
+![logo](./assets/logo.png)
+
+Ultra-simple node app that uploads files from a folder to your ReMarkable Tablet thanks to [RmApi](https://github.com/juruen/rmapi) & [watchman](https://facebook.github.io/watchman/docs/install.html).
+It can be used for simple dnd transfer and can be useful in many other automated applications.
+
+⚠️ Files placed in the folder will be **uploaded then deleted** ⚠️
+
+## Installation
+
+**1 - Clone & install repo**
+
+`git clone https://github.com/hmenzagh/ReMarkable-Sink && cd ReMarkable-Sink && yarn`
+
+**2 - Install [watchman](https://facebook.github.io/watchman/docs/install.html)**
+
+**3 - Install & Configure & Set PATH for [RmApi](https://github.com/juruen/rmapi)**
+
+Make sure you can execute `rmapi` just by typing `rmapi` in your terminal.
+
+**4 - Setup .env file**
+
+```bash
+SINK_FOLDER_PATH='/Users/hmenzagh/Desktop/ReMarkable-Sink' # Must be an absolute path
+REMARKABLE_FOLDER='Sink' # To create in top-level ReMarkable folder
+```
+
+**5 - Do a quick test with `yarn start`**
+
+Done 🎉
+
+## Run at startup
+
+*(This setup is done for MacOS depending on your OS process will differ)*
+
+**1 - Install [forever](https://github.com/foreverjs/forever#readme)**
+
+`npm install forever -g`
+
+**2 - Edit crontab**
+
+```sudo crontab -e```
+
+add
+
+```@reboot forever start /Users/hmenzagh/Projects/ReMarkable-Sink/index.js```
+
+replacing `/Users/hmenzagh/Projects/ReMarkable-Sink/` with your path !
+
---
+++ src/index.js
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ;::      ::::::::    */
+/*   index.js                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmenzagh <hmenzagh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/16 20:10:06 by hmenzagh          #+#    #+#             */
+/*   Updated: 2020/11/16 22:36:12 by hmenzagh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import fs from 'fs';
+import _ from 'lodash';
+import child from 'child_process';
+import watchman from 'fb-watchman';
+
+const { SINK_FOLDER_PATH } = process.env;
+const { REMARKABLE_FOLDER } = process.env;
+
+const client = new watchman.Client();
+
+// ====================================================== //
+// ================== Initiate Watchman ================= //
+// ====================================================== //
+
+client.capabilityCheck({
+	optional: [],
+	required: ['relative_root'],
+},
+(error) => {
+	if (error) {
+		console.error('Error initiating watch:', error);
+		return;
+	}
+	client.command(
+		['watch-project', SINK_FOLDER_PATH],
+		(error, resp) => {
+			if (error) { console.error('Error initiating watch:', error); return; }
+			if ('warning' in resp) { console.log('warning: ', resp.warning); }
+
+			const { watch } = resp;
+			// ~~~~~~~~ Subscribe to Watchman ~~~~~~~~ //
+			client.command(
+				['clock', watch],
+				(error, resp) => {
+					if (error) { console.error('Failed to query clock:', error); return; }
+
+					const subPrefs = {
+						expression: ['allof', ['match', '*.pdf']],
+						fields: ['name', 'exists'],
+						since: resp.clock,
+					};
+
+					client.command(
+						['subscribe', watch, 'PDFSubscription', subPrefs],
+						() => {
+							console.log('Watch established on', watch);
+						},
+					);
+				},
+			);
+		},
+	);
+});
+
+// ====================================================== //
+// =================== Process Changes ================== //
+// ====================================================== //
+
+// Needed for multiple file uploads
+let filesCache = [];
+
+const processFiles = _.debounce((files) => {
+	if (files.length) {
+		const RmApi = child.spawn('rmapi');
+		// ~~~~~~~~~~~~~ Upload Files ~~~~~~~~~~~~ //
+		for (let i = 0; i < files.length; i++) {
+			const file = files[i];
+			RmApi.stdin.write(`put "${SINK_FOLDER_PATH}/${file}" "/${REMARKABLE_FOLDER}"\n`);
+		}
+		RmApi.on('exit', (code) => {
+			// ~~~~~~~~~~~~~ Delete Files ~~~~~~~~~~~~ //
+			files.forEach(async (file) => {
+				fs.unlinkSync(`${SINK_FOLDER_PATH}/${file}`);
+			});
+			filesCache = [];
+			if (code !== 0)
+				throw new Error(`RmApi exited with error code: ${code}`);
+		});
+		RmApi.stdin.end();
+	}
+}, 2000);
+
+client.on('subscription', (resp) => {
+	if (resp.subscription !== 'PDFSubscription') return;
+
+	// ~~~~~~~~~ Update Files to Push ~~~~~~~~ //
+	const files = resp.files.reduce((acc, curr) =>
+		(curr.exists ? { ...acc, toAdd: [...acc.toAdd, curr.name] } : { ...acc, toDel: [...acc.toDel, curr.name] }),
+	{ toAdd: [], toDel: [] });
+	filesCache = _.without(_.uniq([...filesCache, ...files.toAdd]), ...files.toDel);
+
+	processFiles(filesCache);
+});
+
+
+client.on('subscription', (resp) => {
+	if (resp.subscription !== 'PDFSubscription') return;
+
+	// ~~~~~~~~~ Update Files to Push ~~~~~~~~ //
+	const files = resp.files.reduce((acc, curr) =>
+		curr.exists ? {...acc, toAdd: [...acc.toAdd, curr.name]} : {...acc, toDel: [...acc.toDel, curr.name]},
+		{ toAdd: [], toDel: [] })
+	filesCache = _.without(_.uniq([...filesCache, ...files.toAdd]), ...files.toDel)
+
+	processFiles(filesCache)
+});
