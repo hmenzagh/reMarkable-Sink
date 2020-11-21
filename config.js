b286827e26c228e47e21389a9afc97e59ce617c0
Hugo Menzaghi <hmenzagh@MacBook-Pro-de-Hugo.local>
2020-11-21 13:11:26 +0100

    config example

---
+++ config.js
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ;::      ::::::::    */
+/*   config.js                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmenzagh <hmenzagh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/19 14:53:33 by hmenzagh          #+#    #+#             */
+/*   Updated: 2020/11/21 13:10:43 by hmenzagh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+export default [
+	{
+		path: '/Users/hmenzagh/Desktop/ReMarkable-Sink',
+		reMarkableFolder: 'Sink',
+	},
+	{
+		path: '/Users/hmenzagh/.remarkable/ScanBot',
+		reMarkableFolder: 'ScanBot',
+	},
+]
